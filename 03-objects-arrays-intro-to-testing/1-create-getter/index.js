3e4dc3a6d5815df170722eeb92804466c2147f8b
Dmitry Solovov <solovovdv@gmail.com>
2022-11-03 16:40:37 +0300

    Модуль 3. Задача 1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,13 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    const arrPath = path.split('.');
+    return (object) => {
+      let currentObject = object;
+      arrPath.forEach((arrPathElement) =>{
+        if (currentObject === undefined) return;
+        currentObject = currentObject[arrPathElement];
+      });
+      return currentObject;
+    }
 }
