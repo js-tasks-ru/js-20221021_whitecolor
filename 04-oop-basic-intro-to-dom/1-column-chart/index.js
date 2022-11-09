c6c4fe4085da77b18c32ed1ba45f554ef32f2611
Dmitry Solovov <solovovdv@gmail.com>
2022-11-09 10:40:03 +0300

    Модуль 4. Задача 1

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -7,17 +7,14 @@ export default class ColumnChart {
     this.value = value;
     this.link = link;
     this.formatHeading = formatHeading;
-
     this.render();
   }
 
   getTitleTemplate() {
     let link = '';
-
     if (this.link) {
       link = `<a href="/${this.link}" class="column-chart__link">View all</a>`;
     }
-
     return `
         <div class="column-chart__title">
         Total ${this.label && this.label}
@@ -29,24 +26,20 @@ export default class ColumnChart {
   getHeaderTemplate() {
     const intlValue = new Intl.NumberFormat().format(this.value);
     const headerData = this.formatHeading ? this.formatHeading(intlValue) : intlValue;
-
     return `<div data-element="header" class="column-chart__header">${headerData}</div>`;
   }
 
   getBodyTemplate() {
     const maxValue = Math.max(...this.data);
     const scaleValue = this.chartHeight / maxValue;
-
     const cols = this.data.map((value) => {
       const chartValue = Math.floor(scaleValue * value);
       const chartPercent = (value / maxValue * 100).toFixed(0);
 
       return `<div style="--value: ${chartValue}" data-tooltip="${chartPercent}%"></div>`
     })
-
     return `<div data-element="body" class="column-chart__chart">${cols.join('')}</div>`;
   }
-
 
   getChartTemplate() {
     return `
@@ -62,12 +55,10 @@ export default class ColumnChart {
     const wrapper = document.createElement("div");
     wrapper.className = "column-chart";
     wrapper.style.cssText = `--chart-height: ${this.chartHeight}`;
-
     if (!this.data || this.data.length === 0) {
       wrapper.classList.add("column-chart_loading");
     }
     wrapper.innerHTML = this.getChartTemplate();
-
     this.element = wrapper;
   }
 
@@ -78,7 +69,6 @@ export default class ColumnChart {
 
   destroy() {
     this.remove();
-
     this.element = null;
     this.data = null;
     this.label = null;
